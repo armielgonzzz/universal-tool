62426d64457898873fb976cee0024a0e92a46352
armiel <armielgonzales@gmail.com>
2024-10-28 14:49:08 -0500

    added download url for .zip file

--- main.py
+++ main.py
@@ -27,9 +27,7 @@ def get_latest_release():
         for asset in release_data['assets']:
             if asset['name'] == 'main.exe':
                 main_exe_url = asset['browser_download_url']
-
-            if asset['name'] == 'Source code (zip)':
-                source_zip_url = asset['browser_download_url']
+                source_zip_url = asset['zipball_url']
         
         return version, main_exe_url, source_zip_url
     return None, None, None
